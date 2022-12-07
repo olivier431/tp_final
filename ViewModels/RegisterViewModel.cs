eb9430152ccc8120edb7f8fd9cc4b25fdd81a256
Olivier Lefebvre <oli_43@hotmail.fr>
2022-12-07 10:37:47 -0500

    navigate -> Back to login

--- ViewModels/RegisterViewModel.cs
+++ ViewModels/RegisterViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tp_final.Commands;
 using tp_final.Stores;
 
 namespace tp_final.ViewModels
@@ -10,9 +11,16 @@ namespace tp_final.ViewModels
     internal class RegisterViewModel : BaseViewModel
     {
         private readonly NavigationStore navigationStore;
+        public DelegateCommand BackToLoginCommand { get; set; }
 
         public RegisterViewModel(NavigationStore _navigationStore) {
             navigationStore = _navigationStore;
+            BackToLoginCommand = new DelegateCommand(BackToLogin);
+        }
+
+        public void BackToLogin()
+        {
+            navigationStore.CurrentViewModel = new LoginViewModel(navigationStore);
         }
     }
 }
