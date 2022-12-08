7ca677bb0be90732e8b70de53f29d2450efdfa1c
Olivier Lefebvre <oli_43@hotmail.fr>
2022-12-08 14:39:39 -0500

    Add Navigation to album and admin

--- ViewModels/AlbumViewModel.cs
+++ ViewModels/AlbumViewModel.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tp_final.Stores;
 
 namespace tp_final.ViewModels
 {
-    class AlbumViewModel
+    class AlbumViewModel : BaseViewModel
     {
+        private readonly NavigationStore navigationStore;
+
+        public AlbumViewModel(NavigationStore _navigationStore) {
+            navigationStore = _navigationStore;
+        }
     }
 }
--- ViewModels/MainPlayerViewModel.cs
+++ ViewModels/MainPlayerViewModel.cs
@@ -11,11 +11,24 @@ namespace tp_final.ViewModels
     class MainPlayerViewModel : BaseViewModel
     {
         private readonly NavigationStore navigationStore;
+        public DelegateCommand GoToAdminCommand { get; set; }
+        public DelegateCommand GoToAlbumCommand { get; set; }
 
         public MainPlayerViewModel(NavigationStore _navigationStore)
         {
             navigationStore = _navigationStore;
-            
+            GoToAdminCommand = new DelegateCommand(GoToAdmin);
+            GoToAlbumCommand = new DelegateCommand(GoToAlbum);
+
+        }
+
+        public void GoToAdmin()
+        {
+            navigationStore.CurrentViewModel = new AdminUserViewModel(navigationStore);
+        }
+        public void GoToAlbum()
+        {
+            navigationStore.CurrentViewModel = new AlbumViewModel(navigationStore);
         }
     }
 }
