417d14d77346466515fc8bd721d3eb3e704adbc6
Olivier Lefebvre <oli_43@hotmail.fr>
2022-12-09 11:18:04 -0500

    add navigation and binding for username and password for login

--- ViewModels/AlbumViewModel.cs
+++ ViewModels/AlbumViewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
+using tp_final.Commands;
 using tp_final.Services;
 using tp_final.Stores;
 
@@ -16,10 +17,12 @@ namespace tp_final.ViewModels
         ICollectionView albumViewSource;
         TestDataServices testDataServices = new TestDataServices();
         private readonly NavigationStore navigationStore;
-
+        public DelegateCommand GoToAdminCommand { get; set; }
+        
         public AlbumViewModel(NavigationStore _navigationStore) {
             TuneViewSource = CollectionViewSource.GetDefaultView(testDataServices.LesTunes);
             navigationStore = _navigationStore;
+            GoToAdminCommand = new DelegateCommand(GoToAdmin);
         }
 
         public ICollectionView TuneViewSource
@@ -30,6 +33,11 @@ namespace tp_final.ViewModels
                 tuneViewSource = value;
                 OnPropertyChanged();
             }
+        }
+
+        public void GoToAdmin()
+        {
+            navigationStore.CurrentViewModel = new AdminUserViewModel(navigationStore);
         }
     }
 }
--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -3,20 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using tp_final.Commands;
 using tp_final.Stores;
+using tp_final.Views;
 
 namespace tp_final.ViewModels
 {
     internal class LoginViewModel : BaseViewModel
     {
         private readonly NavigationStore navigationStore;
+        private string username;
+        private string password;
         public DelegateCommand GoToRegisterCommand { get; set; }
         public DelegateCommand LoginCommand { get; set; }
         public LoginViewModel(NavigationStore _navigationStore) {
             navigationStore = _navigationStore;
             GoToRegisterCommand = new DelegateCommand(GoToRegister);
             LoginCommand = new DelegateCommand(Login);
+            
+        }
+
+        public string Username { 
+            get => username; 
+            set => username = value;
+        }
+
+        public string Password
+        {
+            get => password;
+            set => password = value;
         }
 
         public void GoToRegister()
@@ -26,6 +42,10 @@ namespace tp_final.ViewModels
 
         public void Login()
         {
+
+            string username = Username;
+            string password = Password;
+            MessageBox.Show(Username + Password);
             navigationStore.CurrentViewModel = new MainPlayerViewModel(navigationStore);
         }
     }
