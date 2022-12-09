9e81ff844a7f61c0295ef5f444cc53e704d7e072
Samuel Cossette <reliantmink267@gmail.com>
2022-12-09 11:20:07 -0500

    Ajout nav Album To Admin

--- ViewModels/AlbumViewModel.cs
+++ ViewModels/AlbumViewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
+using tp_final.Commands;
 using tp_final.Services;
 using tp_final.Stores;
 
@@ -15,13 +16,22 @@ namespace tp_final.ViewModels
         ICollectionView tuneViewSource;
         ICollectionView playlistViewSource;
 
+        public DelegateCommand GoToAdminCommand { get; set; }
+
         TestDataServices testDataServices = new TestDataServices();
         private readonly NavigationStore navigationStore;
 
         public AlbumViewModel(NavigationStore _navigationStore) {
+
             TuneViewSource = CollectionViewSource.GetDefaultView(testDataServices.LesTunes);
             PlaylistViewSource = CollectionViewSource.GetDefaultView(testDataServices.LesPlaylists);
+
             navigationStore = _navigationStore;
+            GoToAdminCommand = new DelegateCommand(GoToAdmin);
+        }
+        public void GoToAdmin()
+        {
+            navigationStore.CurrentViewModel = new AdminUserViewModel(navigationStore);
         }
 
         public ICollectionView TuneViewSource
