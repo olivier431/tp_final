fca89aa78687561fc39c82984cedc284640624b9
Felix <carife2002@gmail.com>
2022-12-12 10:55:54 -0500

    i

---
+++ Services/StringContent.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace MarthaService
+{
+    internal class StringContent
+    {
+        private string param;
+        private Encoding uTF8;
+        private string v;
+
+        public StringContent(string param, Encoding uTF8, string v)
+        {
+            this.param = param;
+            this.uTF8 = uTF8;
+            this.v = v;
+        }
+    }
+}
